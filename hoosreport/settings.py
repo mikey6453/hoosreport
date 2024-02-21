89ce5644f9c47eea3b70603a0273cd67df0b4775
Michael <tvq3bn@virginia.edu>
2024-02-21 07:15:44 -0500

    add postgresqlDB

--- hoosreport/settings.py
+++ hoosreport/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,6 +103,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
