ca63ab799ecb477d4914c23124d9ef15e1c3c3b8
Michael <tvq3bn@virginia.edu>
2024-02-21 04:52:26 -0500

    add social account providers

--- hoosreport/settings.py
+++ hoosreport/settings.py
@@ -49,6 +49,16 @@ INSTALLED_APPS = [
     "allauth.socialaccount.providers.google",   
 ]
 
+SOCIALACCOUNT_PROVIDERS = {
+    "google": {
+        "SCOPE": [
+            "profile",
+            "email"
+        ],
+        "AUTH_PARAMS": {"access_type": "online"}
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
