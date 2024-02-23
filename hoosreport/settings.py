7b67651f2210b049f8fb9d1ec832f040d08eb03c
Michael <tvq3bn@virginia.edu>
2024-02-23 00:53:47 -0500

    replace bootstrap5 with django_bootstrap5

--- hoosreport/settings.py
+++ hoosreport/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.google",
-    "bootstrap5",
+    'django_bootstrap5',
 ]
 
 SOCIALACCOUNT_PROVIDERS = {
