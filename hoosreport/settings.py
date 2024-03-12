db16bd298d09e1fbffcb046f94bd5863e757a971
Axel-Ahlander <cfr8gs@virginia.edu>
2024-03-11 20:05:06 -0400

    Amazon S3 Implementation

--- hoosreport/settings.py
+++ hoosreport/settings.py
@@ -178,16 +178,16 @@ LOGOUT_REDIRECT_URL = "/"
 
 AWS_ACCESS_KEY_ID = 'AKIA47CRZRQFRB5UAKKF'
 AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'django_s3_integration'
+AWS_STORAGE_BUCKET_NAME = 'b-01bucket'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_STATIC_LOCATION = 'static'
-STATICFILES_STORAGE = 'yourproject.storage_backends.StaticStorage'
+STATICFILES_STORAGE = 'hoosreport.storage_backends.StaticStorage'
 AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
-DEFAULT_FILE_STORAGE = 'yourproject.storage_backends.PublicMediaStorage'
+DEFAULT_FILE_STORAGE = 'hoosreport.storage_backends.PublicMediaStorage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_STATIC_LOCATION}/'
 
 
