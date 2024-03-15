7e2a5776f20fc0931a298403ab015db1ef307098
Michael <tvq3bn@virginia.edu>
2024-03-15 00:39:14 -0400

    add url names

--- googleAuth/urls.py
+++ googleAuth/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path 
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("logout", views.logout_view),
-    path("login", views.login_view),
-    path("signup", views.signup_view),
-    path("submit_report", views.report_view, name='submit_report')
+    path("", views.home, name='home'),
+    path("logout/", views.logout_view, name='logout'),
+    path("login/", views.login_view, name='login'),
+    path("signup/", views.signup_view, name='signup'),
+    path("submit_report/", views.report_view, name='submit_report')
 ]
