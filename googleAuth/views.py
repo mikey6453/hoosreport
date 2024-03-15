aa20d8cd9281873d1fe79c1a97ea689ef0911e24
Michael <tvq3bn@virginia.edu>
2024-03-15 01:35:14 -0400

    implement signin page

--- googleAuth/views.py
+++ googleAuth/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+from django.http import HttpResponseRedirect
 
 def home(request):
     user = request.user
@@ -17,6 +18,15 @@ def home(request):
     return render(request, "googleAuth/home.html", context)
 
 def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, "socialaccount/login.html", {'error': 'Invalid username or password.'})
     return render(request, "socialaccount/login.html")
 
 def logout_view(request):
