4046f3f8fdca45487ba5ff962f14060a87a21825
mikey6453 <tvq3bn@virginia.edu>
2025-03-25 19:11:03 -0400

    replace secret key with environment variables

--- hoosreport/settings.py
+++ hoosreport/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import dj_database_url
 import os
+from dotenv import load_dotenv 
+load_dotenv()  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = 'django-insecure-3ue8=+3-07p0w@%7k0avtwz#hv0*wqkt$u=c6z(i6_08kx)iu8'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -178,8 +180,8 @@ LOGOUT_REDIRECT_URL = "/"
 
 # Amazon S3 Buckets Configurations
 
-AWS_ACCESS_KEY_ID = 'AKIAS45UCTENS4CXUIEG'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'project-b-01'
 AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
